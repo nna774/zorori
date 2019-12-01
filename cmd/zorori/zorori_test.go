ed7d0e549c769d297e4bf579f9f820ba814f192f
Kugayama Nana <nonamea774@gmail.com>
2019-12-02 03:21:01 +0900

    add integrated test

---
+++ cmd/zorori/zorori_test.go
@@ -0,0 +1,42 @@
+package main_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/nna774/zorori/resolver/doh"
+)
+
+const ()
+
+func TestDoHAResolve(t *testing.T) {
+	DoHResolvers := []struct {
+		name string
+		uri  string
+	}{
+		{name: "google", uri: "https://dns.google/dns-query"},
+		{name: "iij", uri: "https://public.dns.iij.jp/dns-query"},
+	}
+	for _, r := range DoHResolvers {
+		domains := []struct {
+			domain string
+			ip     net.IP
+		}{
+			{domain: "example.com", ip: net.ParseIP("93.184.216.34")},
+			{domain: "ushio.compute.kitashirakawa.dark-kuins.net", ip: net.ParseIP("192.50.220.189")},
+			{domain: "ushio.c.k.dark-kuins.net", ip: net.ParseIP("192.50.220.189")},
+		}
+		for _, d := range domains {
+			t.Run(r.name+"-"+d.domain, func(t *testing.T) {
+				resolver := doh.NewDoHResolver(r.uri)
+				res, err := resolver.AResolve(d.domain)
+				if err != nil {
+					t.Errorf("err should be nil: %v", err)
+				}
+				if !d.ip.Equal(res.IP()) {
+					t.Errorf("expect: %v, but got %v", d.ip, res.IP())
+				}
+			})
+		}
+	}
+}
